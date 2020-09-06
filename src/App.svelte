27359a30377e9c51e4a7885bc8b35a6cd5af66bb
William FISK <william.fisk@insead.edu>
2020-09-06 20:04:42 +0200

    commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,36 @@
 <script>
-	import Store from './Store';
+	let x = 5;
+  let y = 7;
+  let z;
+	updateZ();
 
-	let x = new Store(5);
-	let y = new Store(17);
-	$: z = $x + $y;
+	function handleXChange(event) {
+    x = parseInt(event.target.value,10);
+    updateZ();
+  }
 
-	function incX() {
-		$x++;
-	}
-	function incY() {
-		$y++;
-	}
+  function handleYChange(event) {
+		y = parseInt(event.target.value,10);
+    updateZ();
+  }
+  
+  function updateZ(){
+    z = x + y;
+  }
 </script>
 
 <div class="container mt-5">
-	<p>The value of x is {$x} <button on:click={incX}>Inc x</button></p>
+  <div class="mb-3">
+    <span>x</span>
+    <input type="number" value={x} on:input={handleXChange} />
+  </div>
 
-	<p>The value of y is {$y} <button on:click={incY}>Inc y</button></p>
+  <div class="mb-3">
+    <span>y</span>
+    <input type="number" value={y} on:input={handleYChange} />
+  </div>
 
-	<p>The value of z is {z}</p>
+  <p>z = {z}</p>
 </div>
 
 <style global type="text/scss">
