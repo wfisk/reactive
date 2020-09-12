2c3188e19802500401ddde9aa1467e069299cb55
William FISK <william.fisk@insead.edu>
2020-09-12 09:37:18 +0200

    commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,25 @@
 <script>
-	let x = 15;
+  let x = 15;
   let y = 7;
   $: z = x + y;
 
-	function handleXInput(event) {
-    x = parseInt(event.target.value,10);
+  function handleXInput(event) {
+    x = parseInt(event.target.value, 10);
   }
 
   function handleYInput(event) {
-		y = parseInt(event.target.value,10);
+    y = parseInt(event.target.value, 10);
   }
-  
 </script>
 
-<div class="container mt-5">
-  <div class="mb-3">
-    <span>x</span>
-    <input type="number" value={x} on:input={handleXInput} />
-  </div>
-
-  <div class="mb-3">
-    <span>y</span>
-    <input type="number" value={y} on:input={handleYInput} />
-  </div>
-
-  <p>z = {z}</p>
+<div class="mb-3">
+  <span>x</span>
+  <input type="number" value={x} on:input={handleXInput} />
 </div>
 
+<div class="mb-3">
+  <span>y</span>
+  <input type="number" value={y} on:input={handleYInput} />
+</div>
 
+<p>z = {z}</p>
