5356ad1f7bb2ee2e494f0b02c6bcb54554eae6bb
William FISK <william.fisk@insead.edu>
2020-09-13 20:31:47 +0200

    commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,27 @@
 <script>
-  import x from './xStore';
-  import y from './yStore';
-  import Header from './Header.svelte';
+  import x from "./xStore";
+  import y from "./yStore";
+  import Header from "./Header.svelte";
 
   $: z = $x + $y;
 
-  function handleXInput(event){
-    let value = parseInt( event.target.value );
+  function handleXInput(event) {
+    let value = parseInt(event.target.value);
     $x = value;
   }
-  
-  function handleYInput(event){
-    let value = parseInt( event.target.value );
+
+  function handleYInput(event) {
+    let value = parseInt(event.target.value);
     $y = value;
   }
-  
 </script>
 
-<div class="container mt-5">
-  <Header />
-  <div class="mb-3">
-    <span>x</span>
-    <input type="number" value={$x} on:input={handleXInput} />
-  </div>
+<Header />
 
-  <div class="mb-3">
-    <span>y</span>
-    <input type="number" value={$y}  on:input={handleYInput}/>
-  </div>
+<span>x</span>
+<input type="number" value={$x} on:input={handleXInput} />
 
-  <p>z = {z}</p>
-</div>
+<span>y</span>
+<input type="number" value={$y} on:input={handleYInput} />
 
+<p>z = {z}</p>
