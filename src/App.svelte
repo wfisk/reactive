bbd8d8dc05d5e3c7608f7081e02294dbaa0f9c47
William FISK <william.fisk@insead.edu>
2020-09-13 20:34:56 +0200

    commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,36 +1,27 @@
 <script>
-  import GameState from './GameState';
-  
-  let gameState = GameState.listen('default');
+  import GameState from "./GameState";
+
+  let gameState = GameState.listen("default");
 
   $: z = $gameState ? $gameState.x + $gameState.y : 0;
 
-  function handleXInput( event ){
-    let value = parseInt( event.target.value );
+  function handleXInput(event) {
+    let value = parseInt(event.target.value);
     $gameState.update({ x: value });
   }
 
-  function handleYInput( event ){
-    let value = parseInt( event.target.value );
+  function handleYInput(event) {
+    let value = parseInt(event.target.value);
     $gameState.update({ y: value });
   }
-
 </script>
 
-<div class="container mt-5">
+{#if $gameState}
+  <span>x</span>
+  <input type="number" value={$gameState.x} on:input={handleXInput} />
 
-  {#if $gameState }
-    <div class="mb-3">
-      <span>x</span>
-      <input type="number" value={$gameState.x}  on:input={handleXInput}/>
-    </div>
+  <span>y</span>
+  <input type="number" value={$gameState.y} on:input={handleYInput} />
 
-    <div class="mb-3">
-      <span>y</span>
-      <input type="number" value={$gameState.y}  on:input={handleYInput}/>
-    </div>
-
-    <p>z = {z}</p>
-   {/if} 
-</div>
-
+  <p>z = {z}</p>
+{/if}
